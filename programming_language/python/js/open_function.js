29e345b41d459c5545ce0db238349be7cced08e2
Mich <captain@blacklotus.fritz.box>
2018-06-30 11:19:25 +0200

    Initial project

---
+++ programming_language/python/js/open_function.js
@@ -0,0 +1,49 @@
+/* ---------------------------------------------- */
+/*                   LOOPS                        */
+/* ---------------------------------------------- */
+function open_MathOperator() {
+    window.open("script/1.Math_Operator.html");
+}
+
+function open_String() {
+    window.open("script/script_example.html");
+}
+
+function open_Sets() {
+    window.open("script/script_example.html");
+}
+
+function open_List() {
+    window.open("script/script_example.html");
+}
+
+function open_Tuples() {
+    window.open("script/script_example.html");
+}
+ /* ---------------------------------------------- */
+ /*                   LOOPS                        */
+ /* ---------------------------------------------- */
+
+function open_ForLoop() {
+    window.open("script/script_example.html");
+}
+
+function open_WhileLoop() {
+    window.open("script/script_example.html");
+}
+
+function open_IfStatement() {
+    window.open("script/script_example.html");
+}
+
+function open_Range() {
+    window.open("script/script_example.html");
+}
+
+function open_BreakContinue() {
+    window.open("script/script_example.html");
+}
+
+function open_Execption() {
+    window.open("script/script_example.html");
+}

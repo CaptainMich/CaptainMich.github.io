9b889add07db08cf4e8a7a5d99935bf0fec81bd6
Mich <michele.salvo96@gmail.com>
2018-07-27 19:38:08 +0200

    Add Android page; no code yet

---
+++ programming_language/android/js/open_function.js
@@ -0,0 +1,6 @@
+/* ---------------------------------------------- */
+/*                   TEMPLATE                     */
+/* ---------------------------------------------- */
+function open_MathOperator() {
+    window.open("code/datatypes/1-Math_Operator.html");
+}

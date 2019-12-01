79eb07b0ec751336e5fb701bc943001a25521dd5
Mich <michele.salvo96@gmail.com>
2019-12-01 16:20:46 +0100

    Add new sidebar; some minor bug fixed

--- programming_language/python/js/code_open_function.js
+++ programming_language/python/js/code_open_function.js
@@ -2,50 +2,50 @@
 /*                  DATATYPES                     */
 /* ---------------------------------------------- */
 function open_MathOperator() {
-    window.open("code/datatypes/1-Math_Operator.html");
+    window.open("code/datatypes/1-Math_Operator.html", "_self");
 }
 
 function open_String() {
-    window.open("code/datatypes/2-String.html");
+    window.open("code/datatypes/2-String.html", "_self");
 }
 
 function open_Sets() {
-    window.open("code/datatypes/3-Sets.html");
+    window.open("code/datatypes/3-Sets.html", "_self");
 }
 
 function open_List() {
-    window.open("code/datatypes/4-Lists.html");
+    window.open("code/datatypes/4-Lists.html", "_self");
 }
 
 function open_Tuples() {
-    window.open("code/datatypes/5-Tuples.html");
+    window.open("code/datatypes/5-Tuples.html", "_self");
 }
  /* ---------------------------------------------- */
  /*                   LOOPS                        */
  /* ---------------------------------------------- */
 
 function open_ForLoop() {
-    window.open("code/loops/1-For_Loop.html");
+    window.open("code/loops/1-For_Loop.html", "_self");
 }
 
 function open_WhileLoop() {
-    window.open("code/loops/2-While_Loop.html");
+    window.open("code/loops/2-While_Loop.html", "_self");
 }
 
 function open_IfStatement() {
-    window.open("code/loops/3-If_Statement.html");
+    window.open("code/loops/3-If_Statement.html", "_self");
 }
 
 function open_Range() {
-    window.open("code/loops/4-Range.html");
+    window.open("code/loops/4-Range.html", "_self");
 }
 
 function open_BreakContinue() {
-    window.open("code/loops/5-Break_Continue_Statement.html");
+    window.open("code/loops/5-Break_Continue_Statement.html", "_self");
 }
 
 function open_Execption() {
-    window.open("code/loops/6-Exception.html");
+    window.open("code/loops/6-Exception.html", "_self");
 }
 
 /* ---------------------------------------------- */
@@ -53,7 +53,7 @@ function open_Execption() {
 /* ---------------------------------------------- */
 
 function open_Functions() {
-   window.open("code/functions/1-Functions.html");
+   window.open("code/functions/1-Functions.html", "_self");
 }
 
 /* ---------------------------------------------- */
@@ -61,7 +61,7 @@ function open_Functions() {
 /* ---------------------------------------------- */
 
 function open_Class() {
-   window.open("code/OOP/1-Class.html");
+   window.open("code/OOP/1-Class.html", "_self");
 }
 
 /* ---------------------------------------------- */
@@ -69,5 +69,5 @@ function open_Class() {
 /* ---------------------------------------------- */
 
 function open_GUI_Form_and_Widget() {
-   window.open("code/GUI/1-GUI_Form_and_Widget.html");
+   window.open("code/GUI/1-GUI_Form_and_Widget.html", "_self");
 }

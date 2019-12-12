73d59cbaba79b9bc44005321e4d458d1d8d1b3b5
Mich <michele.salvo96@gmail.com>
2019-12-12 21:42:01 +0100

    Add picoCTF writup - binary; add new prism themes

--- programming_language/basc/js/open_function.js
+++ programming_language/basc/js/open_function.js
@@ -1,6 +1,26 @@
 /* ---------------------------------------------- */
 /*                   LOOPS                        */
 /* ---------------------------------------------- */
-function open_picoCTF2019() {
-    window.open("CTF/picoCTF2019/picoCTF2019.html", "_self");
+function open_picoCTF2019_binary() {
+    window.open("CTF/picoCTF2019/binary.html", "_self");
+}
+
+function open_picoCTF2019_cryptography() {
+    window.open("CTF/picoCTF2019/cryptography.html", "_self");
+}
+
+function open_picoCTF2019_forensics() {
+    window.open("CTF/picoCTF2019/forensics.html", "_self");
+}
+
+function open_picoCTF2019_general_skill() {
+    window.open("CTF/picoCTF2019/general_skill.html", "_self");
+}
+
+function open_picoCTF2019_reverse() {
+    window.open("CTF/picoCTF2019/reverse.html", "_self");
+}
+
+function open_picoCTF2019_web_exploitation() {
+    window.open("CTF/picoCTF2019/web_exploitation.html", "_self");
 }
